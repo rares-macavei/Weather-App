7f32e2a0b92d7b2d7633e1461d28d79b6189ac9f
Rares Macavei <raresmacavei15@gmail.com>
2023-04-06 22:50:55 +0300

    v1.1 - config the api

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,22 +1,63 @@
+import Descriptions from "./components/Descriptions";
+import { useEffect, useState } from "react";
+import { getFormattedWeatherData } from "./weatherService";
+import logo from './assets/logo.png'
+
 function App() {
+  const [city, setCity] = useState("Cluj-Napoca");
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      const data = await getFormattedWeatherData(city);
+      setWeather(data);
+    };
+
+    fetchWeatherData();
+  }, [city]);
+
+
+  const enterKeyPressed = (e) => {
+    if (e.keyCode === 13) {
+      setCity(e.currentTarget.value);
+      e.currentTarget.blur();
+    }
+  };
+
   return (
     <div className="app">
       <div className="overlay">
-        <div className="container">
-          <div className="section section__inputs">
-            <input type="text" name="city" placeholder="Enter city..." />
-          </div>
-          <div className="section section__temperature">
-            <div className="icon">
-              <h3>London, GB</h3>
-              <img src="https://openweathermap.org/img/wn/02d@2x.png" alt="weatherIcon" />
-              <h3>Cloudy</h3>
+        
+        {weather && (
+          <div className="container">
+            <a href="#" className="logo">
+              <img src={logo} alt="logo" className="" />
+            </a>
+            <div className="section section__inputs">
+              <input
+                onKeyDown={enterKeyPressed}
+                type="text"
+                name="city"
+                placeholder="Enter City..."
+                className="search-view"
+              />
             </div>
-            <div className="temperature">
-              <h1>34&deg;C</h1>
+
+            <div className="section section__temperature">
+              <div className="icon">
+                <h3>{`${weather.name}, ${weather.country}`}</h3>
+                <img src={weather.iconURL} alt="weatherIcon" />
+                <h3>{weather.description}</h3>
+              </div>
+              <div className="temperature">
+                <h1>{`${weather.temp.toFixed()} °C`}</h1>
+              </div>
             </div>
+
+            {/* bottom description */}
+            <Descriptions weather={weather}/>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
