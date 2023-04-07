68be8698d173861912b5a7c0906b45d505fdfc29
Rares Macavei <raresmacavei15@gmail.com>
2023-04-08 01:33:13 +0300

    v1.3

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -2,6 +2,9 @@ import Descriptions from "./components/Descriptions";
 import { useEffect, useState } from "react";
 import { getFormattedWeatherData } from "./weatherService";
 import logo from './assets/logo.png'
+import { BiSearch } from 'react-icons/bi'
+import { BsGithub } from 'react-icons/bs'
+import { BsLinkedin } from 'react-icons/bs'
 
 function App() {
   const [city, setCity] = useState("Cluj-Napoca");
@@ -30,32 +33,45 @@ function App() {
         
         {weather && (
           <div className="container">
-            <a href="#" className="logo">
-              <img src={logo} alt="logo" className="" />
-            </a>
-            <div className="section section__inputs">
-              <input
-                onKeyDown={enterKeyPressed}
-                type="text"
-                name="city"
-                placeholder="Enter City..."
-                className="search-view"
-              />
-            </div>
-
-            <div className="section section__temperature">
-              <div className="icon">
-                <h3>{`${weather.name}, ${weather.country}`}</h3>
-                <img src={weather.iconURL} alt="weatherIcon" />
-                <h3>{weather.description}</h3>
+            <div className="header">
+              <a href="#" className="logo">
+                <img src={logo} alt="logo" className="" />
+              </a>
+              <div className="search-view">
+                <div className="search-wrapper">
+                  <input type="text" 
+                  onKeyDown={enterKeyPressed} 
+                  name="city" 
+                  placeholder="Enter a city..." autocomplete="off" className="search-field"/>
+                  <BiSearch className='m-icon leading-icon'/>
+                </div>
               </div>
-              <div className="temperature">
-                <h1>{`${weather.temp.toFixed()} °C`}</h1>
+              <div className="link-container">
+                <a href="https://github.com/rares-macavei/Weather-App" target="_blank">
+                  <button className="link-div">
+                    <BsGithub className="link-btn"/>
+                  </button>
+                </a>
+                <a href="https://www.linkedin.com/in/raresmacavei/" target="_blank">
+                  <button className="link-div">
+                    <BsLinkedin className="link-btn"/>
+                  </button>
+                </a>
               </div>
             </div>
+              <div className="card card-sm highlight">
+                <div className="icon">
+                  <h3>{`${weather.name}, ${weather.country}`}</h3>
+                  <img src={weather.iconURL} alt="weatherIcon" />
+                  <h3>{weather.description}</h3>
+                </div>
+                <div className="temperature">
+                  <h1>{`${weather.temp.toFixed()} °C`}</h1>
+                </div>
+              </div>
 
-            {/* bottom description */}
-            <Descriptions weather={weather}/>
+              {/* bottom description */}
+              <Descriptions weather={weather}/>
           </div>
         )}
       </div>
